1675a6d32c70fe4b0733805e308580871aca5571
Fpsska <fpsska1337@gmail.com>
2023-01-05 12:39:35 +0300

    init: initial commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
+// /. imports
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {}
 });
 
 export type AppDispatch = typeof store.dispatch;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import '../../assets/styles/_reset.scss';
+import '../../assets/styles/style.scss';
+
+// /. imports
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1 style={{ color: '#000' }}>Hello world</h1>
+        </div>
+    );
+};
+
+export default App;
